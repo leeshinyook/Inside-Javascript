9c32ab7e59dd5eb11987998f7a6e2fa0d3a29b9a
shinyooklee <tlsdbr97@gmail.com>
2020-01-07 15:47:31 +0900

    Add : closure

---
+++ ch5/closure.js
@@ -0,0 +1,19 @@
+// function outerFunc() {
+//   var x = 10;
+//   var innerFunc = function() {
+//     console.log(x);
+//   };
+//   return innerFunc;
+// }
+// var inner = outerFunc();
+// inner();
+
+function outerFunc(arg1, arg2) {
+  var local = 8;
+  function innerFunc(innerArg) {
+    console.log((arg1 + arg2) / (innerArg + local));
+  }
+  return innerFunc;
+}
+var exam1 = outerFunc(2, 4);
+exam1(2);
